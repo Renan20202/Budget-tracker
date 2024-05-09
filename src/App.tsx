854a84c46e90553a661d2d56483a22861a29ef6a
Renan20202 <renansanchezrubio21@gmail.com>
2024-05-09 03:13:54 -0600

    little change

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
     <>
       <header className="max-h-72  font-bold px-4 py-8 bg-gradient-to-r from-[#5DADE2] via-[#3498DB] to-[#2E86C1]  text-white">
         <h1 className="uppercase text-center font-black text-4xl text-white">
-          Budget Tracker
+          Budget Tracker By Reh
         </h1>
       </header>
 
